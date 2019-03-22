7e29fa05b841bb3523b66cbd20362ae67e9511ef
Arin Trongsantipong <arintrongs@gmail.com>
2019-03-22 14:21:09 +0700

    Last Kuy Kuy

--- src/chat-page/chatRoom.js
+++ src/chat-page/chatRoom.js
@@ -20,7 +20,7 @@ class ChatRoom extends Component {
             input: '',
             data: []
         };
-        this.socket = io('http://35.198.245.254');
+        this.socket = io('http://5b0457b7.ngrok.io');
         this.socket.on('addNewChat', data => {
             if (data.groupid === this.props.gid) {
                 this.setState({ data: [...this.state.data, data] });
@@ -45,7 +45,7 @@ class ChatRoom extends Component {
             groupid: gid
         };
         await axios
-            .post('http://35.198.245.254/parallel/getChatByGroupID', data)
+            .post('http://5b0457b7.ngrok.io/parallel/getChatByGroupID', data)
             .then(response => {
                 this.setState({ data: response.data });
                 this.sortData();
